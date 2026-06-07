8a23b324109c8a7febb5c7193a205443df42f6f3
Dariana <mpdari24@gmail.com>
2026-06-06 20:41:53 -0400

    fix: agregar format requerido en access_log_settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,8 +178,19 @@ resource "aws_apigatewayv2_stage" "default" {
   name        = "$default"
   auto_deploy = true
 
-  access_log_settings {
+access_log_settings {
     destination_arn = aws_cloudwatch_log_group.lambda_logs.arn
+    format = jsonencode({
+      requestId      = "$context.requestId"
+      sourceIp       = "$context.identity.sourceIp"
+      requestTime    = "$context.requestTime"
+      protocol       = "$context.protocol"
+      httpMethod     = "$context.httpMethod"
+      resourcePath   = "$context.resourcePath"
+      routeKey       = "$context.routeKey"
+      status         = "$context.status"
+      responseLength = "$context.responseLength"
+    })
   }
 
   tags = {
